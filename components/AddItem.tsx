74e528447d9e036589972233976c82e825b8a7c5
Afolabi Abdulsamad <fullendmaestro@gmail.com>
2024-06-05 14:50:19 +0100

    new gist added

--- components/AddItem.tsx
+++ components/AddItem.tsx
@@ -23,7 +23,7 @@ export default function AddItem({
       <div className="space-y-12">
         <div className="pb-4 border-b border-gray-200">
           <h2 className="text-xl font-semibold leading-1 text-gray-900">
-            New Item
+            New Holla 🗣️
           </h2>
 
           <div className="mt-4 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
@@ -32,7 +32,7 @@ export default function AddItem({
                 htmlFor="name"
                 className="block text-sm font-medium leading-6 text-gray-900"
               >
-                Name
+                Gist
               </label>
               <div className="mt-2">
                 <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-primary sm:max-w-md">
@@ -41,7 +41,7 @@ export default function AddItem({
                     name="name"
                     id="name"
                     className="block flex-1 border-0 bg-transparent py-1.5 pl-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                    placeholder="test item"
+                    placeholder="voice out 🗣️"
                     value={itemName}
                     onChange={(e) => setItemName(e.target.value)}
                   />
@@ -65,7 +65,7 @@ export default function AddItem({
           type="submit"
           className="rounded-md bg-primary px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-primary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary"
         >
-          Save
+          Share
         </button>
       </div>
     </form>
