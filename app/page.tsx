5344ced5c779e0640ba8e44766dad9fdccb4ac81
Afolabi Abdulsamad <fullendmaestro@gmail.com>
2024-06-05 14:32:19 +0100

    changed home page content

--- app/page.tsx
+++ app/page.tsx
@@ -54,15 +54,20 @@ export default async function Home() {
         <div className="mx-auto max-w-2xl pt-16 sm:pt-24 lg:pt-32">
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              EdgeDB Next.js Starter
+              GIST HUB
             </h1>
             <p className="mt-6 text-base leading-7 text-gray-600">
-              Welcome to the EdgeDB Next.js Starter. This starter is designed to
-              help you get up and running with EdgeDB and Next.js quickly. It
-              includes a basic setup for authentication, EdgeDB schema, and a UI
-              to get you started. Below are some next steps to help you get up
-              to speed.
+              Welcome to GISTHUB. A place where anyone anyplace anytime can
+              share a gist or be gisted 🤝
             </p>
+            <Link
+              href="/home"
+              className="text-sm font-semibold leading-6 text-gray-800"
+            >
+              <button className="rounded-md bg-primary-100 px-4 py-2 ring-2 ring-inset ring-primary-500">
+                Get Started
+              </button>
+            </Link>
           </div>
         </div>
         <div className="mx-auto max-w-2xl pt-4 sm:pt-8 lg:pt-12">
